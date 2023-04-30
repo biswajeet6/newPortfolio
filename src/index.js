1bd32a352073dad6bb3d2517226c58d4fa9b44f8
biswajeet6 <78801379+biswajeet6@users.noreply.github.com>
2023-04-30 23:49:33 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-NZYG9J5QY3"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
+  gtag('config', 'G-NZYG9J5QY3');
+</script>
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
