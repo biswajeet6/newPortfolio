472144954696fc647484f9df85cfba186315440c
biswajeet6 <78801379+biswajeet6@users.noreply.github.com>
2023-04-30 23:50:22 +0100

    Update ProjectItem.js

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,6 +1,14 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-NZYG9J5QY3"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
+  gtag('config', 'G-NZYG9J5QY3');
+</script>
 function ProjectItem({ image, name, id }) {
   const navigate = useNavigate();
   return (
