736342cf09d35b6e21f83d4c7b962bcbf5fd10fe
biswajeet6 <78801379+biswajeet6@users.noreply.github.com>
2023-04-30 23:50:10 +0100

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,15 @@ import React,{useEffect,useState} from 'react'
 import {Link, useLocation} from 'react-router-dom' 
 import '../styles/Navbar.css'
 import ReorderIcon from "@material-ui/icons/Reorder"
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-NZYG9J5QY3"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
+  gtag('config', 'G-NZYG9J5QY3');
+</script>
 function Navbar() {
   const [expandNavbar, setExpandNavbar] = useState(false); 
 
